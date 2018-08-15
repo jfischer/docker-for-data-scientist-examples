a6f3f8c56888de56381205cc54db731814f28f11
Jeff Fischer <jeff@data-ken.org>
2018-08-14 18:02:06 -0700

    Minor enhancements

--- README.rst
+++ README.rst
@@ -1,2 +1,4 @@
 These are examples from my PyBay 2018 talk:
 `Docker for Data Scientists <https://pybay.com/speaker/jeff-fischer/#talk-65>`__.
+
+For a full explanation, see my blog series at https://data-ken.org/docker-for-data-scientists-part1.html.
--- workflow-4/run.sh
+++ workflow-4/run.sh
@@ -23,6 +23,14 @@ for f in nodes-*.csv
 popd
 
 echo "Running the database import"
+echo 'docker run -it --rm \'
+echo '  --volume=`pwd`/data:/data \'
+echo '  --volume=`pwd`/graph-import-files:/imports \'
+echo '  --volume=`pwd`/logs:/logs \'
+echo '  --env=NEO4J_AUTH=neo4j/test \'
+echo '  neo4j:3.4 bin/neo4j-admin import \'
+echo '    $NODES_ARGS \'
+echo '    $EDGES_ARGS'
 docker run -it --rm \
    --volume=`pwd`/data:/data \
    --volume=`pwd`/graph-import-files:/imports \
@@ -31,6 +39,12 @@ docker run -it --rm \
    neo4j:3.4 bin/neo4j-admin import $NODES_ARGS $EDGES_ARGS
 
 echo "Starting the database server"
+echo 'docker run -d \'
+echo '   --volume=`pwd`/data:/data \'
+echo '   --volume=`pwd`/logs:/logs \'
+echo '   --env=NEO4J_AUTH=neo4j/test \'
+echo '   -p 7474:7474 -p 7687:7687 \'
+echo '   --name pybay-neo4j neo4j:3.4'
 docker run -d \
    --volume=`pwd`/data:/data \
    --volume=`pwd`/logs:/logs \
